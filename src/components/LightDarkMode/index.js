c6341f6fdd7887d8772f9119641c48c75c9da35e
bhargav09052004 <ananthsaibhargavtata@gmail.com>
2023-08-02 14:15:35 +0000

    Errors

---
+++ src/components/LightDarkMode/index.js
@@ -0,0 +1,18 @@
+import {Component} from 'react'
+import './index.css'
+
+class LightDarkMode extends Component {
+  state = {isDarkMode: true}
+
+  render() {
+    const {isDarkMode} = this.state
+    return (
+      <div>
+        <h1> Click To change Mode </h1>
+        ((isDarkMode)?<button type="button">Dark Mode</button>:
+        <button type="button">Light Mode</button>))
+      </div>
+    )
+  }
+}
+export default LightDarkMode
